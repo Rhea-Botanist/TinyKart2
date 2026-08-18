13a7e9d0961cf9af0ccdda646c5c07bb6d0337f6
Ubuntu <markpopkov006@gmail.com>
2026-08-18 00:00:30 -0400

    YahboomInterface and gitignore finally

--- build/tf2_transforms/CMakeFiles/tf2_transforms_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/tf2_transforms_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/tf2_transforms_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
